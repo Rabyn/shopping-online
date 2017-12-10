a760838508ec890e92ce990766c86b5bf61f0681
Rabindra Shrestha <rabynshrestha@gmail.com>
2017-12-09 19:42:06 -0600

    Running the Project in Apache complete

---
+++ onlineshopping/src/main/java/com/training/onlineshopping/controller/PageController.java
@@ -0,0 +1,16 @@
+package com.training.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = {"/","/home","/index"})
+	public ModelAndView index() {
+		ModelAndView mav = new ModelAndView("page");
+		mav.addObject("greeting", "Welcome to Spring Web MVC");
+		return mav;
+	}
+}
