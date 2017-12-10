3dffbef5485f1ee857232ab52ad65bd1e74d3ea0
Rabindra Shrestha <rabynshrestha@gmail.com>
2017-12-09 18:14:27 -0600

    adding project in Repo

---
+++ shoppingbackend/src/main/java/com/training/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.training.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
